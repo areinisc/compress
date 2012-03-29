7f86a780d55c302f9e11813cd86b97838cd6a016
Alex Reinisch <areinisc@slu.edu>
2012-03-29 10:06:02 -0500

    skeleton-ing

--- compress.cpp
+++ compress.cpp
@@ -8,6 +8,15 @@
 #include<vector>
 using namespace std;
 
+int W(-1), T(-1), count(0);         // Width, Threshold, and count
+
 void main() {
+    while (W != 0) {
+        ++count;                    // increment image count
+        cin >> W;
+        cin >> T;
+        cout << "Image " << count << ":" << endl;
+        //RECURSION!
+    }
     return 0;
 }
