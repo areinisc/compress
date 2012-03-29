ce7a9e9f90998e9c8473b6ad6be9c4d8d968aed0
Alex Reinisch <areinisc@slu.edu>
2012-03-29 12:49:48 -0500

    lossy signature developing

--- compress.cpp
+++ compress.cpp
@@ -9,7 +9,14 @@
 using namespace std;
 
 int W(-1), T(-1), n(0);         // Width, Threshold, and n
-vector<string> image;           // store image as 2D string vector
+vector<string> image;           // store image as string vector
+
+void lossy( const int& W,       // image width
+            const int& T,       // compression threshold
+            const vector<string>& image // image as string vector
+        ) {
+    cout << "RECURSION!" << endl;
+}
 
 int main() {
     cin >> W;
@@ -18,10 +25,10 @@ int main() {
         ++n;                    // increment image count
         cin >> T;               // find Threshold
         for (vector<double>::size_type i = 0; i < W; i++) {
-            cin >> image[i];
+            cin >> image[i];    // fill string vector with image data
         }
         cout << "Image " << n << ":" << endl;
-        //lossy(W, T, image);//RECURSION!
+        lossy(W, T, image);     // RECURSION!
         image.clear();          // clears image for next loop
         cin >> W;               // finds next width(0 for exit)
     }
